6bac86dd7ff2274fd41616084b1a47c4c9a99d91
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-04-14 04:42:25 -0300

    cambios

---
+++ ejercicio_9.js
@@ -0,0 +1,8 @@
+const numeros = [1, 2, 3, 4, 5]; // Array de ejemplo
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i]; // Sumar cada elemento del array
+}
+
+console.log("La suma de los elementos del array es: " + suma);
