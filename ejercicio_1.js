8c2365e3042bc5234d6a3744fabb926ce842dde3
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-04-17 03:00:07 -0300

    cambios

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,18 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Ingresa el primer número: ", (input1) => {
-  const numero1 = parseFloat(input1);
-
-  rl.question("Ingresa el segundo número: ", (input2) => {
-    const numero2 = parseFloat(input2);
-    const resultado = numero1 + numero2;
-
-    console.log("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
-    rl.close();
-  });
-});
+const numero1 = 5;
+const numero2 = 5;
+const resultado = numero1 + numero2;
+console.log("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
