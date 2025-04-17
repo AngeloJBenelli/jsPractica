0f2adf2323b5b48e7490e4b23901a0049e395488
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-04-17 03:03:36 -0300

    cambios

--- ejercicio_4.js
+++ ejercicio_4.js
@@ -1,20 +1,7 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Ingresa un número: ", (input) => {
-  const numero = parseInt(input);
-
-  if (isNaN(numero)) {
-    console.log("Por favor, ingresa un número válido.");
-  } else if (numero % 2 === 0) {
+const numero = 10;
+if (numero % 2 === 0) { // Verifica si el número es par
+  // El operador % devuelve el resto de la división entre el número y 2
     console.log("El número es par.");
   } else {
     console.log("El número no es par.");
   }
-
-  rl.close();
-});
