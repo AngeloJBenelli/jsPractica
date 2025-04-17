bb30c61b96ec47f5f005c0f50de1f3cccc07a216
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-04-17 03:08:26 -0300

    cambios

--- ejercicio_6.js
+++ ejercicio_6.js
@@ -1,32 +1,12 @@
-const readline = require('readline');
+const numero1 = 1;
+const numero2 = 10;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const inicio = Math.min(numero1, numero2) + 1;// Se suma 1 para no incluir los números extremos
+const fin = Math.max(numero1, numero2);// Se toma el máximo para no incluir los números extremos
+const numerosSeparados = [];
 
-rl.question("Ingresa el primer número: ", (input1) => {
-  const numero1 = parseInt(input1);
+for (let i = inicio; i < fin; i++) {
+ numerosSeparados.push(i);
+}
 
-  rl.question("Ingresa el segundo número: ", (input2) => {
-    const numero2 = parseInt(input2);
-
-    if (isNaN(numero1) || isNaN(numero2)) {
-      console.log("Por favor, ingresa números válidos.");
-    } else if (numero1 === numero2 || Math.abs(numero1 - numero2) === 1) {
-      console.log("No hay números que los separen.");
-    } else {
-      const inicio = Math.min(numero1, numero2) + 1;
-      const fin = Math.max(numero1, numero2);
-      const numerosSeparados = [];
-
-      for (let i = inicio; i < fin; i++) {
-        numerosSeparados.push(i);
-      }
-
-      console.log("Los números que los separan son: " + numerosSeparados.join(", "));
-    }
-
-    rl.close();
-  });
-});
+console.log("Los números que los separan son: " + numerosSeparados + ".");
