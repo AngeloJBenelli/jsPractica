0c9d707ac9c30e55d9fc4d8f2b0831c53a364599
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-04-17 03:05:27 -0300

    cambios

--- ejercicio_5.js
+++ ejercicio_5.js
@@ -1,21 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Ingresa una palabra: ", (palabra) => {
-  rl.question("Ingresa un número: ", (inputNumero) => {
-    const numero = parseInt(inputNumero);
-
-    if (isNaN(numero) || numero <= 0) {
-      console.log("Por favor, ingresa un número válido mayor a 0.");
-    } else {
-      const resultado = palabra.repeat(numero);
-      console.log("Resultado: " + resultado);
-    }
-
-    rl.close();
-  });
-});
+const palabra = "KUMOKO";
+const numero = 1000;
+const resultado = (palabra + " ").repeat(numero);
+console.log (resultado);
